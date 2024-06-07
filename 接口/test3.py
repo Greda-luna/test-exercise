d46c5a85729ece2eb35a4689cc7089c4a474a048
Greda_luna <97780369+Greda-luna@users.noreply.github.com>
2024-06-07 23:13:35 +0800

    Update test3.py

--- 接口/test3.py
+++ 接口/test3.py
@@ -1,7 +1,7 @@
 import requests
 url = 'https://contest.gitlab.ceba.ceshiren.com/api/v4/projects/id1162/issues/issue_iid变量/'
 headers = {
-    'PRIVATE-TOKEN':'glpat-QfZs-RpyGyseE3edSjZk'
+    'PRIVATE-TOKEN':'令牌'
 }
 data = {
     'id':1162,
